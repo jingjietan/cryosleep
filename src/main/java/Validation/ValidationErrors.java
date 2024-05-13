9bbe207183f5aa875c053b0009d0560acf026962
jingjietan <112614622+jingjietan@users.noreply.github.com>
2024-05-13 14:47:28 +0800

    verifier

--- src/main/java/Validation/ValidationErrors.java
+++
@@ -1,8 +0,0 @@
-package Validation;
-
-public enum ValidationErrors {
-    INSTRUMENT_NOT_FOUND,
-    MISMATCHED_CURRENCY,
-    INVALID_LOT_SIZE,
-    POSITION_CHECK_FAILED
-}
--- src/main/java/Validation/Validator.java
+++
@@ -1,7 +0,0 @@
-package Validation;
-
-import Common.OrderData;
-
-public interface Validator {
-    boolean verify(OrderData orderData);
-}
